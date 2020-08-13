7e483a939f7c324a1ccf5ecffb25059d190c66ea
ggahui <dbrkgml97@nate.com>
2020-08-13 21:36:22 +0900

    binarysearch cutTree

---
+++ Algorithm/binaryserch/CutTree_2805.java
@@ -0,0 +1,60 @@
+package Algorithm.binaryserch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CutTree_2805 {
+
+    static int[] treeHeight;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        treeHeight = new int[N];
+
+        int left = 0, right = 0, middle;
+        long m_max;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            treeHeight[i] = Integer.parseInt(st.nextToken());
+
+            if(right < treeHeight[i])
+                right = treeHeight[i];
+        }
+
+        while (left <= right) {
+            middle = (right+left+1)/2;
+            //System.out.println(middle);
+
+            m_max = cutTree(middle);
+            //System.out.println(m_max + " m max");
+
+            if(M <= m_max) {
+                left = middle + 1;
+            }else if (M > m_max){
+                right = middle -1;
+            }
+        }
+
+        System.out.println(right);
+    }
+
+    public static long cutTree(int middle) {
+        long sum = 0;
+
+        //나무를 자름
+        for(int i=0; i<treeHeight.length; i++) {
+            if(treeHeight[i] > middle) {
+                sum += (treeHeight[i] - middle);
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ Algorithm/greedy/Assignment_13904.java
@@ -0,0 +1,25 @@
+package Algorithm.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Assignment_13904 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+
+        }
+        st = new StringTokenizer(br.readLine());
+
+    }
+}
