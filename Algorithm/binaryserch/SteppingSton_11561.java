e505a0e229f7052d93a4a50addfc9202d347d065
ggahui <dbrkgml97@nate.com>
2020-08-17 22:15:39 +0900

    union-find

--- Algorithm/binaryserch/SteppingSton_11561.java
+++ Algorithm/binaryserch/SteppingSton_11561.java
@@ -1,11 +1,10 @@
 package Algorithm.binaryserch;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SteppingSton_11561 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         long T = sc.nextLong();
@@ -13,9 +12,9 @@ public class SteppingSton_11561 {
         long left, right, middle;
         long ans;
 
-        BigInteger k, BN, NN;
+        BigInteger k, BN;
 
-        for(long i=0; i<T; i++) {
+        for (long i = 0; i < T; i++) {
             N = sc.nextLong();
             right = N;
             left = 1;
@@ -24,16 +23,15 @@ public class SteppingSton_11561 {
             while (left <= right) {
                 middle = (left + right) / 2;
 
-
-
-                k = new BigInteger("middle*(middle + 1)/2");
+                k = new BigInteger(BigInteger.valueOf(middle * (middle + 1) / 2).toString());
                 BN = BigInteger.valueOf(N);
 
-                if(k.compareTo(BN) <= 0) {
-                    ans =  middle;
-                    left =  middle + 1;
-                }
-                else {
+                //System.out.println("BN " + BN);
+
+                if (k.compareTo(BN) <= 0) {
+                    ans = middle;
+                    left = middle + 1;
+                } else {
                     right = middle - 1;
                 }
             }
