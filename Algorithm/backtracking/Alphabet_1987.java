1e45a78a7c4bdb4ea64a05a34d0041ce866b1e2b
ggahui <dbrkgml97@nate.com>
2020-09-23 23:17:13 +0900

    Solve BackTracking 200923

---
+++ Algorithm/backtracking/Alphabet_1987.java
@@ -0,0 +1,68 @@
+package Algorithm.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Alphabet_1987 {
+
+    static int max = 1;
+    static int[][] alpha;
+    static boolean[] used;
+    static boolean[][] visited;
+
+    static int r, c;
+
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        alpha = new int[r][c];
+        used = new boolean[26];
+        visited = new boolean[r][c];
+
+        for (int i = 0; i < r; i++) {
+            String inputString = br.readLine();
+
+            for (int j = 0; j < inputString.length(); j++) {
+                alpha[i][j] = inputString.charAt(j) - 'A';
+            }
+        }
+
+        visited[0][0] = true;
+        DFS(0, 0, 1);
+        System.out.println(max);
+    }
+
+    public static void DFS(int x, int y, int cnt) {
+
+        max = Math.max(max, cnt);
+        used[alpha[x][y]] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < r && ny >= 0 && ny < c && !visited[nx][ny]) {
+
+                if (used[alpha[nx][ny]])
+                    continue;
+
+
+                visited[nx][ny] = true;
+                DFS(nx, ny, cnt + 1);
+                visited[nx][ny] = false;
+            }
+        }
+
+        used[alpha[x][y]] = false;
+    }
+}
--- Algorithm/backtracking/CreatePassword.java
+++ Algorithm/backtracking/CreatePassword.java
@@ -3,27 +3,66 @@ package Algorithm.backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class CreatePassword {
+
+    static ArrayList<String> arr;
+    static int L, C;
+    static String[] pwAlphabet;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int L = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        String[] pwAlphabet = new String[L];
+        pwAlphabet = new String[C];
+        arr = new ArrayList<>();
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<L; i++) {
-            pwAlphabet[i] = st.nextToken();
-        }
+
+        String inputString = br.readLine();
+        pwAlphabet = inputString.split(" ");
 
         Arrays.sort(pwAlphabet);
 
+        DFS(0, 0, 0,0,"");
+
+        Collections.sort(arr);
+
+        for(String str : arr) {
+            System.out.println(str);
+        }
+    }
+
+    public static void DFS(int cnt, int start, int consonant, int vowel, String str) {
+        String temp = str;
+
+        if(cnt == L) {
+            if(consonant >=2 && vowel >=1)
+                arr.add(temp);
+
+            return;
+        }
+
+        if(cnt + C - start < L)
+            return;
+
+        //해당 index를 포함하지 않는다.
+        DFS(cnt, start+1, consonant, vowel, temp);
+
+        //해당 index 포함
+        if(pwAlphabet[start].equals("a") || pwAlphabet[start].equals("i") || pwAlphabet[start].equals("o")
+        || pwAlphabet[start].equals("e") || pwAlphabet[start].equals("u"))
+            vowel++;
+        else
+            consonant++;
+
+        DFS(cnt+1, start+1, consonant, vowel, temp+pwAlphabet[start]);
 
     }
 }
