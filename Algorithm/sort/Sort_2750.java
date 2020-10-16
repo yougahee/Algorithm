4d9fa2227ab279360f040e5bd1231bf36ce7696a
ggahui <dbrkgml97@nate.com>
2020-10-17 01:13:56 +0900

    Resolve sort problem

--- Algorithm/sort/Sort_2750.java
+++ Algorithm/sort/Sort_2750.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Sort {
+public class Sort_2750 {
     public static void main(String[] args) throws Exception {
         int N;
 
