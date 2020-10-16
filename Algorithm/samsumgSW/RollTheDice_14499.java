d548dacf263010b714a045f4d55e0c67c474c6a5
ggahui <dbrkgml97@nate.com>
2020-10-16 16:24:49 +0900

    Solve RolltheDice 201016

--- Algorithm/samsumgSW/RollTheDice_14499.java
+++ Algorithm/samsumgSW/RollTheDice_14499.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+//주사위를 어떻게 굴릴지 혼자 생각해내지 못했음..
+//ㅜㅅㅜ
 public class RollTheDice_14499 {
 
     static int[][] map;
@@ -24,13 +26,6 @@ public class RollTheDice_14499 {
     static int[] dx = {0, 0, -1, 1}; //동서북남
     static int[] dy = {1, -1, 0, 0};
 
-    //가로
-    static int[] row = {6,4,1,3};
-    static int rowPoint = 2;
-    //세로
-    static int[] col = {2,1,5,6};
-    static int colPoint = 1;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -44,7 +39,6 @@ public class RollTheDice_14499 {
 
         map = new int[n][m];
 
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -57,7 +51,49 @@ public class RollTheDice_14499 {
             direction.add(Integer.parseInt(st.nextToken()));
         }
 
+        while (!direction.isEmpty()) {
+            int dir = direction.poll();
+            int nx = x + dx[dir - 1];
+            int ny = y + dy[dir - 1];
 
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+            int top = dice[1];
+            if(dir == 1) {
+                dice[1] = dice[4];
+                dice[4] = dice[6];
+                dice[6] = dice[3];
+                dice[3] = top;
+            }else if(dir == 2) {
+                dice[1] = dice[3];
+                dice[3] = dice[6];
+                dice[6] = dice[4];
+                dice[4] = top;
+            }else if(dir == 3) {
+                dice[1] = dice[5];
+                dice[5] = dice[6];
+                dice[6] = dice[2];
+                dice[2] = top;
+            }else {
+                dice[1] = dice[2];
+                dice[2] = dice[6];
+                dice[6] = dice[5];
+                dice[5] = top;
+            }
+
+            //지도의 칸이 0일 경우,
+            if (map[nx][ny] == 0) {
+                map[nx][ny] = dice[6];
+            }
+            //칸이 0 이 아닌경우
+            else {
+                dice[6] = map[nx][ny];
+                map[nx][ny] = 0;
+            }
+
+            x = nx;
+            y = ny;
+            System.out.println(dice[1]);
+        }
     }
-
 }
