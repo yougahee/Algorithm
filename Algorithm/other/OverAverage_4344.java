0d05a93984cd80d9d39dff8b5dfa828ab2253362
ggahui <dbrkgml97@nate.com>
2020-10-17 01:52:17 +0900

    Resolve SelfNum 201017

---
+++ Algorithm/other/OverAverage_4344.java
@@ -0,0 +1,34 @@
+package Algorithm.other;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class OverAverage_4344 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int size = Integer.parseInt(st.nextToken());
+            int sum =0;
+            int[] student = new int[size];
+            for(int j=0; j<size; j++) {
+                student[j] = Integer.parseInt(st.nextToken());
+                sum += student[j];
+            }
+
+            double avg = (double) sum/ (double) size;
+            int cnt =0;
+            for( int value : student ){
+                if(value > avg) cnt++;
+            }
+
+            double ans = (double) cnt/ (double) size * 100;
+            System.out.println(String.format("%.3f", ans) + "%");
+        }
+    }
+}
