23d60de2d3f26a0f614aba420f1bd31f7ed8607b
ggahui <dbrkgml97@nate.com>
2020-10-26 16:33:22 +0900

    Solve two pointer 201026

---
+++ Algorithm/twopointer/GolombSequence_2038.java
@@ -0,0 +1,14 @@
+package Algorithm.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GolombSequence_2038 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+    }
+}
