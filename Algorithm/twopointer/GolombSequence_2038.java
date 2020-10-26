681d9225e1f2c22264e05db15116ecf28b6e2175
ggahui <dbrkgml97@nate.com>
2020-10-26 21:26:56 +0900

    Solve two pointer 201026

--- Algorithm/twopointer/GolombSequence_2038.java
+++ Algorithm/twopointer/GolombSequence_2038.java
@@ -8,7 +8,23 @@ public class GolombSequence_2038 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        
+        long N = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+        int index = 1;
+        int[] arr = new int[700000];
+        arr[1] = 1;
+
+        while (true) {
+            sum += arr[index];
+
+            if( sum >= N ) {
+                System.out.println(index);
+                break;
+            }
+
+            index++;
+            arr[index] = 1 + arr[index - arr[arr[index-1]]];
+        }
     }
 }
