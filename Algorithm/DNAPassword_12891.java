d531547530a97ebd1bb5ce2cee9878a3d4bfe1b6
ggahui <dbrkgml97@nate.com>
2020-11-02 14:39:15 +0900

    Solve binary search 201102

---
+++ Algorithm/DNAPassword_12891.java
@@ -0,0 +1,19 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DNAPassword_12891 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+
+
+    }
+}
