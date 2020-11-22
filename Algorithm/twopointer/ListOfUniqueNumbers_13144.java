ada312da2308edbf80702ff2c84c6dd7b16fd446
ggahui <dbrkgml97@nate.com>
2020-11-23 00:23:50 +0900

    Solve two pointer 201123

--- Algorithm/twopointer/ListOfUniqueNumbers_13144.java
+++ Algorithm/twopointer/ListOfUniqueNumbers_13144.java
@@ -3,6 +3,10 @@ package Algorithm.twopointer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class ListOfUniqueNumbers_13144 {
@@ -10,16 +14,29 @@ public class ListOfUniqueNumbers_13144 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        boolean[] isChecked = new boolean[n+1];
+        int[] arr = new int[n];
+        Set<Integer> set = new HashSet<>();
+        long sum = 0;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int s=0, e=0;
+        while (true) {
+            if(s == n) break;
 
+            if(e < n && !set.contains(arr[e])) {
+                set.add(arr[e]);
+                e++;
+            } else {
+                sum += set.size();
+                set.remove(arr[s]);
+                s++;
+            }
+        }
 
-
+        System.out.println(sum);
     }
 }
