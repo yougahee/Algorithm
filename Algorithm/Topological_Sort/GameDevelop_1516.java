24a72194ae855e13e834e71932a720a009128881
ggahui <dbrkgml97@nate.com>
2020-11-24 15:50:57 +0900

    Solve Topological Sort 201124

--- Algorithm/Topological_Sort/GameDevelop_1516.java
+++ Algorithm/Topological_Sort/GameDevelop_1516.java
@@ -18,59 +18,44 @@ public class GameDevelop_1516 {
         int[] result = new int[N];
 
         ArrayList<Integer>[] arr = new ArrayList[N];
-        Queue<BuiltTime> queue = new LinkedList();
+        Queue<Integer> queue = new LinkedList();
         for(int i=0; i<N; i++) arr[i] = new ArrayList<>();
 
-        int time, node, prev;
+        int time, node;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
 
             time = Integer.parseInt(st.nextToken());
             building[i][1] = time;
 
-            prev = i;
             while (true) {
                 node = Integer.parseInt(st.nextToken());
                 if (node == -1) break;
 
-                building[node-1][0]++;
-                arr[node-1].add(prev);
-                prev = node-1;
+                building[i][0]++;
+                arr[node-1].add(i);
+            }
+
+            if (building[i][0] == 0) {
+                result[i] = building[i][1];
+                queue.add(i);
             }
         }
 
-
         for(int i=0; i<N; i++) {
-            if (building[i][0] == 0) queue.add(new BuiltTime(i, building[i][1]));
-        }
+            if(queue.isEmpty()) break;
 
-        for(int i=0; i<N; i++) {
-            //이러면 위상정렬이 안만들어지긴 하는데,
-            //여기서는 각자가 만드는 것의 최소값을 구하면 되서 이 처리를 따로 안해줘도 되는 건가?
-            if(queue.isEmpty()) {
-                break;
-            }
-
-            BuiltTime b = queue.poll();
-            for(int j : arr[b.node]) {
+            int b = queue.poll();
+            for(int j : arr[b]) {
+                result[j] = Math.max(result[j], building[j][1] + result[b]);
                 if(--building[j][0] == 0) {
-                    building[j][1] += b.time;
+                    queue.add(j);
                 }
             }
         }
 
-
-        for(int i=0; i<N; i++)
-            System.out.println(building[i][1]);
-
-    }
-
-    static class BuiltTime {
-        int node, time;
-
-        public BuiltTime(int x, int y) {
-            this.node = x;
-            this.time = y;
+        for(int i=0; i<N; i++){
+            System.out.println(result[i]);
         }
     }
 }
